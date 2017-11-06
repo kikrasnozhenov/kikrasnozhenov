3b965a0b5bc69ce7918991207f11cbabc5629e9a
kikrasnozhen <krasnozhenovk@gmail.com>
2017-11-06 12:34:44 +0200

    made px perfect .. created toggle class for filter

--- js/filter.js
+++ js/filter.js
@@ -1,10 +1,9 @@
-// var x = document.getElementsByClassName("filter_item");
-
-function target() { 
-     var el = document.getElementsByClassName("filter_item"); 
-     el.addEventListener("click", function(el){ 
-         [].forEach.call(x, function( el ) {
-         el.className = "filter_target";
-        });
+$(document).ready(function () {
+   
+    $('span.filter_item').click(function() {
+        $(this).toggleClass('filter_choose');
     });
-}
+});
+
+  
+    
---
+++ js/newbar.js
@@ -0,0 +1,7 @@
+// $(document).ready(function () {
+   
+//     $('div.location_details').click(function() {
+//         $(this).toggleClass('persons_contacts_details_toggle');
+//         console.log('dadada')
+//     });
+// });
