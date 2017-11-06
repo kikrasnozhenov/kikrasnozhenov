f928c3d3160be45eca2d893f33275184d1aff1d7
kikrasnozhen <krasnozhenovk@gmail.com>
2017-11-06 12:47:58 +0200

    deleted js file

--- js/newbar.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function () {
-   
-//     $('div.location_details').click(function() {
-//         $(this).toggleClass('persons_contacts_details_toggle');
-//         console.log('dadada')
-//     });
-// });
